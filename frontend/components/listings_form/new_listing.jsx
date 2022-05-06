5e3eeb742dd8b3fb9b464a026e8f99c412aea3f7
Derek <fataldigitz@gmail.com>
2022-05-06 09:19:18 -0400

    remove one debugger

--- frontend/components/listings_form/new_listing.jsx
+++ frontend/components/listings_form/new_listing.jsx
@@ -29,7 +29,6 @@ export default class NewListing extends React.Component{
         const formData = new FormData();
         const latitude = (Math.random()*(40.795199-40.704868))+40.704868;
         const longitude = (Math.random()*(-73.933641+74.017313))-74.017313;
-        debugger;
         if (this.state.title === '' || this.state.description === '' || this.state.address === '' || this.state.city === '' || this.state.state === '' || this.state.zipcode==='' || this.state.max_num_guests === '' || this.state.num_beds === '' || this.state.num_baths === '' || this.state.price_per_night ==='' || this.state.photos.length < 1){
             this.setState({errors: 'fill out all fields bruh'});
         }
