d2efddf066559843d7e7ee9f3c414c7d4b019113
derek <fataldigitz@gmail.com>
2022-06-06 19:43:57 -0400

    comment in validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
     validates :title, :host_id, :address, :city, :state, :zipcode, :latitude, :longitude, :max_num_guests, :num_beds, :num_baths, :description, :price_per_night, presence: true
     validates :address, uniqueness: true
-    # validate :validate_photos
+    validate :validate_photos
 
     belongs_to :host,
         primary_key: :id,
